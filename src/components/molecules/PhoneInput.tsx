4a44e92a77b184da63c2a342664cd72c3457cb57
Nick Charbonneau <yes@hire-nick.dev>
2025-08-23 21:49:58 -0700

    phone input styles

--- src/components/molecules/PhoneInput.tsx
+++ src/components/molecules/PhoneInput.tsx
@@ -30,11 +30,33 @@ export function PhoneInput({
       <div className="relative w-[434px]">
         <PhoneInputBase
           international
-          defaultCountry={defaultCountry}
+          defaultCountry={defaultCountry as any}
           value={value}
-          onChange={onChange}
+          onChange={onChange as any}
           placeholder={placeholder}
-          className={`w-full ${className}`}
+          className={`
+            w-full bg-transparent border border-[#5c5c5c] text-white 
+            placeholder:text-[#949494] rounded-[3px] px-3 py-2
+            focus:outline-none focus:border-white
+            [&_.PhoneInputCountry]:text-white
+            [&_.PhoneInputCountrySelect]:bg-transparent
+            [&_.PhoneInputCountrySelect]:text-white
+            [&_.PhoneInputCountrySelect]:border-none
+            [&_.PhoneInputCountrySelect]:outline-none
+            [&_.PhoneInputCountrySelectArrow]:text-[#949494]
+            [&_.PhoneInputInput]:bg-transparent
+            [&_.PhoneInputInput]:text-white
+            [&_.PhoneInputInput]:border-none
+            [&_.PhoneInputInput]:outline-none
+            [&_.PhoneInputInput]:placeholder:text-[#949494]
+            [&_.PhoneInputCountrySelect__option]:text-white
+            [&_.PhoneInputCountrySelect__option]:bg-[#2d2d2d]
+            [&_.PhoneInputCountrySelect__option--selected]:bg-[#5c5c5c]
+            [&_.PhoneInputCountrySelect__option--selected]:text-white
+            [&_.PhoneInputCountrySelect__option--focused]:bg-[#5c5c5c]
+            [&_.PhoneInputCountrySelect__option--focused]:text-white
+            ${className}
+          `}
         />
       </div>
     </div>
